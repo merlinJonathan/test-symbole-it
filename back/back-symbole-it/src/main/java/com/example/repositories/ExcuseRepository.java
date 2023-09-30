82128cd0a36f06b705445ca0ee1f118eea65f2d8
merlinJonathan <jo_merlin@hotmial.fr>
2023-09-30 13:30:25 +0200

    Suppression du repository jpa pour les excuses au profit de firestore

--- back/back-symbole-it/src/main/java/com/example/repositories/ExcuseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repositories;
-
-import com.example.Model.ExcuseModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExcuseRepository extends JpaRepository<ExcuseModel, Long> {
-}
